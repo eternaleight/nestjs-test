4b60b6a6fbe7df3e416a6e266ddc6e8bf4b50394
eternaleight <eternaleight0@gmail.com>
2022-09-30 12:19:49 +0900

    feat(task): create task entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskModule } from './tasks/tasks.module'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'secret',
+      database: 'mydb-dev',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TaskModule,
+  ],
 })
 
 export class AppModule {}
---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @Column({ default: false })
+  isCompleted: boolean
+
+}
+
