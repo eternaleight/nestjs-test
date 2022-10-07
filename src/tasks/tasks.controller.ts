0c4c3bb713ebea5c58d526f1dcae861903038a75
eternaleight <eternaleight0@gmail.com>
2022-10-07 20:44:27 +0900

    feat(task) :created TasksController

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task';
+import {UpdateTaskDto} from './dto/update-task.dto';
 import { Task } from './tasks.entity';
 import { TasksServices } from './tasks.service';
 
@@ -11,4 +12,28 @@ export class TasksContoroller {
   create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.TasksServices.create(createTaskDto);
   }
+
+  @Get()
+  findAll(): Promise<Task[]> {
+    return this.TasksServices.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id')id: string): Promise<Task> {
+    return this.TasksServices.findOne(id)
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ):Promise<Task> {
+    return this.TasksServices.update(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.TasksServices.delete(id);
+  }
+
 }
